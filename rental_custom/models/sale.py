aabc2f2d7744cf1204f50806efb5c0352a74a900
Carrysoft <carrysoft@gmail.com>
2024-02-09 19:46:39 +0000

    incorporamos funcion de chequeo de disponibilidad a futuro

--- rental_custom/models/sale.py
+++ rental_custom/models/sale.py
@@ -147,66 +147,66 @@ class SaleOrderLine(models.Model):
         return res
 
     # def _check_rental_availability(self):
-    #     raise ValidationError( str( "hola"))
-    #     res=super(SaleOrderLine,self).product_id_change()
-    #     return res
+    #    raise ValidationError( str( "hola"))
+    #    res=super(SaleOrderLine,self).product_id_change()
+    #    return res
 
-    # def _check_rental_availability(self):
-    #     raise UserError("ok__"+str(  self.product_id.rented_product_id  ))
-    #     self.ensure_one()
-    #     res = {}
-    #     if not self.start_date or not self.end_date or not self.rental_qty:
-    #         return {}
-    #     total_qty = self.product_id.rented_product_id.with_context(
-    #         {"location": self.order_id.warehouse_id.rental_view_location_id.id}
-    #     ).qty_available
-    #     max_ol_qty = self._get_max_overlapping_rental_qty()
-    #     avail_qty = total_qty - max_ol_qty
-    #     self.product_qty_rent=avail_qty
-    #     if self.rental_qty > avail_qty:
-    #         res = self._get_concurrent_orders()
-    #         if total_qty == 0:
-    #             self.concurrent_orders = "none"
-    #         elif res["quotation"] and not res["order"]:
-    #             self.concurrent_orders = "quotation"
-    #         else:
-    #             self.concurrent_orders = "order"
-    #         res["warning"] = {
-    #             "title": _("Not enough stock!"),
-    #             "message": _(
-    #                 "You want to rent %.2f %s but you only "
-    #                 "have %.2f %s available in the selected period."
-    #             )
-    #             % (
-    #                 self.rental_qty,
-    #                 self.product_id.rented_product_id.uom_id.name,
-    #                 avail_qty,
-    #                 self.product_id.rented_product_id.uom_id.name,
-    #             ),
-    #         }
-    #     else:
-    #         self.concurrent_orders = "none"
-    #     return res
+    def _check_rental_availability(self):
+        raise UserError("ok__"+str(  self.product_id.rented_product_id  ))
+        self.ensure_one()
+        res = {}
+        if not self.start_date or not self.end_date or not self.rental_qty:
+            return {}
+        total_qty = self.product_id.rented_product_id.with_context(
+            {"location": self.order_id.warehouse_id.rental_view_location_id.id}
+        ).qty_available
+        max_ol_qty = self._get_max_overlapping_rental_qty()
+        avail_qty = total_qty - max_ol_qty
+        self.product_qty_rent=avail_qty
+        if self.rental_qty > avail_qty:
+            res = self._get_concurrent_orders()
+            if total_qty == 0:
+                self.concurrent_orders = "none"
+            elif res["quotation"] and not res["order"]:
+                self.concurrent_orders = "quotation"
+            else:
+                self.concurrent_orders = "order"
+            res["warning"] = {
+                "title": _("Not enough stock!"),
+                "message": _(
+                    "You want to rent %.2f %s but you only "
+                    "have %.2f %s available in the selected period."
+                )
+                % (
+                    self.rental_qty,
+                    self.product_id.rented_product_id.uom_id.name,
+                    avail_qty,
+                    self.product_id.rented_product_id.uom_id.name,
+                ),
+            }
+        else:
+            self.concurrent_orders = "none"
+        return res
 
-    # def compute_stock(self):
-    #     total_qty=0
-    #     if self.product_id and self.product_id.rented_product_id:
-    #         total_qty = self.product_id.rented_product_id.with_context(
-    #             {"location": self.order_id.warehouse_id.rental_view_location_id.id}
-    #         ).qty_available
-    #         max_ol_qty = self._get_max_overlapping_rental_qty()
-    #         avail_qty = total_qty - max_ol_qty
-    #         self.product_qty_rent=avail_qty
+    def compute_stock(self):
+        total_qty=0
+        if self.product_id and self.product_id.rented_product_id:
+            total_qty = self.product_id.rented_product_id.with_context(
+                {"location": self.order_id.warehouse_id.rental_view_location_id.id}
+            ).qty_available
+            max_ol_qty = self._get_max_overlapping_rental_qty()
+            avail_qty = total_qty - max_ol_qty
+            self.product_qty_rent=avail_qty
 
-    # @api.onchange('product_id')
-    # def product_id_change(self):
-    #     res=super(SaleOrderLine,self).product_id_change()
-    #     self.compute_stock()
-    #     return res
+    @api.onchange('product_id')
+    def product_id_change(self):
+        res=super(SaleOrderLine,self).product_id_change()
+        self.compute_stock()
+        return res
 
-    # @api.onchange("start_date", "end_date", "product_uom")
-    # def onchange_start_end_date(self):
-    #     self.compute_stock()
-    #     res=super(SaleOrderLine,self).onchange_start_end_date()
-    #     return res
+    @api.onchange("start_date", "end_date", "product_uom")
+    def onchange_start_end_date(self):
+        self.compute_stock()
+        res=super(SaleOrderLine,self).onchange_start_end_date()
+        return res
 
