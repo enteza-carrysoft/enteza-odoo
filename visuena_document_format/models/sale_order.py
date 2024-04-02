d4ca4e19e187a43964e20726fcd0fa8ebde44007
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-04-02 14:57:23 +0200

    document format

--- visuena_document_format/models/sale_order.py
+++ visuena_document_format/models/sale_order.py
@@ -7,26 +7,19 @@ from odoo import api, fields, models, _
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-
     @api.onchange('order_line')
     def _onchange_order_line(self):
-        self._get_used_categories()
-
-    @api.model
-    def _get_used_categories(self):
         categories = []
         for line in self.order_line:
             categories.append(line.product_id.categ_id.id)
-        used_categories =self.env["product.category"].search([('id', 'in', categories)])
+        used_categories = self.env["product.category"].search([('id', 'in', categories)])
         self.used_categories = used_categories
-
 
     @api.depends('order_line.product_id.categ_id')
     def _compute_used_categories(self):
         for order in self:
             categories = order.order_line.mapped('product_id.categ_id')
             order.used_categories = [(6, 0, categories.ids)]
-
 
     used_categories = fields.Many2many(
         'product.category',
