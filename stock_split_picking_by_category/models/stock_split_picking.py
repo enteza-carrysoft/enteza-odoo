7283e11a4be99f65612c652f4bc7ce43598f7ba6
enteza-carrysoft <informatica@alquisillas.com>
2024-05-15 23:01:39 +0000

    Se incluye filtro por categoria a la hora de dividir los albaranes

--- stock_split_picking_by_category/models/stock_split_picking.py
+++ stock_split_picking_by_category/models/stock_split_picking.py
@@ -1,18 +1,25 @@
 from odoo import models, fields
 
-class StockSplitPicking(models.TransientModel):
+class StockPicking(models.TransientModel):
     _inherit = "stock.split.picking"
 
-    family_id = fields.Many2one('product.family', string="Product Family")
+#    categ_id = fields.Many2one('product.category', string="Product Category")
 
-    def _apply_selection(self):
-        """Modify to create a picking for all selected moves filtered by product family."""
-        if self.family_id:
-            moves = self.move_ids.filtered(lambda m: m.product_id.family_id == self.family_id)
-        else:
-            moves = self.mapped("move_ids")
-        if moves:
-            new_picking = moves.mapped("picking_id")._split_off_moves(moves)
-            return self._picking_action(new_picking)
-        return False
+    categ_ids = fields.Many2many(
+        comodel_name='product.category',
+        string='Categoria',
+        required=False,
+        help="Select category"
+    )
+
+#    def _apply_selection(self):
+#        """Modify to create a picking for all selected moves filtered by product family."""
+#        if self.category_id:
+#            moves = self.move_ids.filtered(lambda m: m.product_id.categ_id == self.categ_id)
+#        else:
+#            moves = self.mapped("move_ids")
+#        if moves:
+##            new_picking = moves.mapped("picking_id")._split_off_moves(moves)
+#            return self._picking_action(new_picking)
+#        return False
 
