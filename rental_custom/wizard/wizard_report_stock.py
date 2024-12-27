1086ec719cfeda0538bf283b967719e3e3179632
enteza-carrysoft <informatica@alquisillas.com>
2024-12-27 12:46:28 +0000

    .

--- rental_custom/wizard/wizard_report_stock.py
+++ rental_custom/wizard/wizard_report_stock.py
@@ -4,37 +4,6 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
-
-class WizardReportStock(models.TransientModel):
-    _name = "wizards.report.stock"
-    _description = "Product pack line"
-
-
-    def export_action(self):
-        if self.options=='product' and not self.product_ids:
-            raise ValidationError("Indique al menos un articulo")
-        if self.options=='category' and not self.categ_id:
-            raise ValidationError("Indique al menos una clasificacion")
-        if not self.warehouse_id:
-            raise ValidationError("Indique un almacen")
-        product_ids=self.product_ids.ids
-        if self.options=='category' and  self.categ_id:
-            product_ids=self.env['product.product'].search([('categ_id','=',self.categ_id.id),('rental', '=', True),('type', '=','product')]).ids
-        return {
-            'name': 'Reporte Existencias',
-            'type': 'ir.actions.client',
-            'tag': 'rental_dashboard',
-            'context': {'product_ids':product_ids ,'date_start':self.date_start,'date_stop':self.date_stop,'warehouse_id':self.warehouse_id.id}
-        }
-
-    options=fields.Selection([('product','Por Articulo'),('category','Clasificacion')],string="Opcion",required=True)
-    categ_id=fields.Many2one("product.category",string="Categoria")
-    product_ids=fields.Many2many("product.product",string="Productos")
-    date_start=fields.Date(string="Fecha Inicio",required=True)
-    date_stop=fields.Date(string="Fecha Fin",required=True)
-    warehouse_id=fields.Many2one("stock.warehouse",string="Almacen")
-
-
 class WizardCreateSale(models.TransientModel):
     _name = "wizards.create.sale"
     _description = "Product pack line"
@@ -70,10 +39,6 @@ class WizardCreateSale(models.TransientModel):
         origins = ", ".join(p.origin for p in self.picking_ids if p.origin)
         if origins:
             sale_id.message_post(body="Documentos origen: %s" % origins)
-
-        # Opcional, si deseas vincular la orden de venta a los albaranes
-#        for picking in self.picking_ids:
-#            picking.write({'sale_order_rental_id': sale_id.id})
 
         return {
             'name': 'Sale Order',
