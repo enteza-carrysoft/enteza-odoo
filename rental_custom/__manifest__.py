6d75f2b8bc9921cc5409bcd5a8165066aa727f96
enteza-carrysoft <informatica@alquisillas.com>
2025-02-01 13:40:36 +0000

    cambios en widget

--- rental_custom/__manifest__.py
+++ rental_custom/__manifest__.py
@@ -15,16 +15,6 @@
     "data": [
         'views/sale_order_views.xml',
     ],
-    'assets': {
-    'web.assets_qweb': [
-        'rental_custom/static/src/widgets/rental_availability_widget.xml',
-    ],
-    'web.assets_backend': [
-        'rental_custom/static/src/widgets/rental_availability_widget.js',
-    ],
-    },
-    "demo": [
-    ],
     "installable": True,
     "auto_install": False,
     "application": False,
