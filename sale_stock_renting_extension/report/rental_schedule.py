7ec95d20f1de850380e0e2d751c5f6a3fd72372a
enteza-carrysoft <informatica@alquisillas.com>
2025-02-16 19:19:42 +0000

    .

--- sale_stock_renting_extension/report/rental_schedule.py
+++ sale_stock_renting_extension/report/rental_schedule.py
@@ -5,3 +5,10 @@ class RentalSchedule(models.Model):
 
     partner_shipping_id = fields.Many2one('sale.order', 'Lugar',readonly=True)
 
+    def _select(self) -> SQL:
+        return SQL("""%s,
+            lot_info.lot_id as lot_id,
+            s.warehouse_id as warehouse_id,
+            s.partner_shipping_id as partner_shipping_id
+        """, super(RentalSchedule, self)._select())
+
