a103899427d7560901278301df90f04d6c69c6f6
enteza-carrysoft <informatica@alquisillas.com>
2025-02-16 19:35:51 +0000

    .

--- sale_stock_renting_extension/report/rental_schedule.py
+++ sale_stock_renting_extension/report/rental_schedule.py
@@ -8,8 +8,6 @@ class RentalSchedule(models.Model):
 
     def _select(self) -> SQL:
         return SQL("""%s,
-            lot_info.lot_id as lot_id,
-            s.warehouse_id as warehouse_id,
             s.partner_shipping_id as partner_shipping_id
         """, super(RentalSchedule, self)._select())
 
