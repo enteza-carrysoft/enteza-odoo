18380c2b76fcf28329c466cff7a863df7b06b253
enteza-carrysoft <informatica@alquisillas.com>
2025-02-16 13:40:35 +0000

    .

--- sale_stock_renting_extension/report/rental_schedule.py
+++ sale_stock_renting_extension/report/rental_schedule.py
@@ -1,6 +1,8 @@
-from odoo import api,fields, models, tools
+from odoo import fields, models
+form odoo.tools import SQL
 
 class RentalSchedule(models.Model):
     _inherit = "sale.rental.schedule"
+
     partner_shipping_id = fields.Many2one('sale.order', 'Lugar',readonly=True)
 
