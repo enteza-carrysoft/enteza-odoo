f8ae7b0b329b86610b8e532981482bf8edbc4019
enteza-carrysoft <informatica@alquisillas.com>
2025-03-01 11:01:16 +0000

    .

--- stock_missing_units/models/stock_picking.py
+++ stock_missing_units/models/stock_picking.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api
 
-class StockMoveLine(models.Model):
-    _inherit = 'stock.move.line'
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
     
     missing_units = fields.Integer(string='Missing Units', default=0)
     
@@ -10,22 +10,4 @@ class StockMoveLine(models.Model):
         for line in self:
             if line.missing_units:
                 line.qty_done = line.product_uom_qty - line.missing_units
-
-class StockMove(models.Model):
-    _inherit = 'stock.move'
-    
-    missing_units = fields.Integer(
-        string='Missing Units',
-        compute='_compute_missing_units',
-        store=True,
-        readonly=False
-    )
-    
-    @api.depends('move_line_ids.missing_units')
-    def _compute_missing_units(self):
-        for move in self:
-            if move.move_line_ids:
-                move.missing_units = sum(move.move_line_ids.mapped('missing_units'))
-            else:
-                move.missing_units = 0
 
